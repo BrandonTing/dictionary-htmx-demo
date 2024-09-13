4b68c558cf070fd89d6ce8da8d4d24a0ac0207dc
Brandon Ting <brandonting@kkcompany.com>
2024-09-13 17:05:35 +0800

    ignore db for now

--- internal/server/server.go
+++ internal/server/server.go
@@ -15,7 +15,7 @@ import (
 type Server struct {
 	port int
 
-	db database.Service
+	// db database.Service
 }
 
 func NewServer() *http.Server {
@@ -23,7 +23,7 @@ func NewServer() *http.Server {
 	NewServer := &Server{
 		port: port,
 
-		db: database.New(),
+		// db: database.New(),
 	}
 
 	// Declare Server config
